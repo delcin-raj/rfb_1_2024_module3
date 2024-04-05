24ca464306765393d99aefa017df6415987e4dbd
delcin-raj <delcinraj@gmail.com>
2024-04-05 23:21:04 +0530

    rpc_client with timeout

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
-use std::{env, sync::Mutex};
+use std::{env, sync::Mutex, time};
 
-use bitcoincore_rpc::{Auth, Client, RpcApi};
+use bitcoincore_rpc::{json, jsonrpc::{self, serde_json}, Auth, Client, RpcApi};
 use chrono::Duration;
 #[macro_use]
 extern crate lazy_static;
 
 lazy_static! {
     static ref RPC_CLIENT: Client = {
+        dotenv::dotenv().ok();
         let rpc_url: String = env::var("BITCOIN_RPC_URL").expect("BITCOIN_RPC_URL must be set");
         let rpc_user: String = env::var("BITCOIN_RPC_USER").expect("BITCOIN_RPC_USER must be set");
-        let rpc_password: String = env::var("BITCOIN_RPC_PASSWORD").expect("BITCOIN_RPC_PASSWORD must be set");
+        let rpc_password: String =
+            env::var("BITCOIN_RPC_PASSWORD").expect("BITCOIN_RPC_PASSWORD must be set");
         Client::new(&rpc_url, Auth::UserPass(rpc_user, rpc_password)).unwrap()
     };
 }
@@ -29,6 +31,24 @@ fn number_of_transactions(block_height: u64) -> u16 {
 fn main() {
     // you can use rpc_client here as if it was a global variable
     // println!("{:?}", res);
-    let rpc_client = &*RPC_CLIENT;
-    rpc_client.get_block_hash(234);
+    const TIMEOUT_UTXO_SET_SCANS: time::Duration = time::Duration::from_secs(60 * 8); // 8 minutes
+    dotenv::dotenv().ok();
+        let rpc_url: String = env::var("BITCOIN_RPC_URL").expect("BITCOIN_RPC_URL must be set");
+        let rpc_user: String = env::var("BITCOIN_RPC_USER").expect("BITCOIN_RPC_USER must be set");
+        let rpc_password: String =
+            env::var("BITCOIN_RPC_PASSWORD").expect("BITCOIN_RPC_PASSWORD must be set");
+
+    let custom_timeout_transport = jsonrpc::simple_http::Builder::new()
+        .url(&rpc_url)
+        .expect("invalid rpc url")
+        .auth(rpc_user, Some(rpc_password))
+        .timeout(TIMEOUT_UTXO_SET_SCANS)
+        .build();
+    let custom_timeout_rpc_client =
+        jsonrpc::client::Client::with_transport(custom_timeout_transport);
+
+    let rpc_client = Client::from_jsonrpc(custom_timeout_rpc_client);
+    let res: json::GetTxOutSetInfoResult =
+        rpc_client.get_tx_out_set_info(None, None, None).unwrap();
+    println!("{:?}", res);
 }
